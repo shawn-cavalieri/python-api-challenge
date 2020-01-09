7716d6bd218e0460eda4d970f681ae6bffd20ba2
Shawn Cavalieri <shawn.cavalieri@yahoo.com>
2020-01-09 00:16:12 -0500

    final changes

---
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,38 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# OpenWeatherMap API Key\n",
+    "weather_api_key = \"210f01173d6275ddba9238415e53b312\"\n",
+    "\n",
+    "# Google API Key\n",
+    "g_key = \"AIzaSyDCeNuF7pj0D1F4u0AtGv5l_LZeRKR73WM\"\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
